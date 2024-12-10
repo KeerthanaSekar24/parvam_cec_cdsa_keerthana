a622937d65f86cb11888a759e5c76616a61e1750
KeerthanaSekar24 <keert602@gmail.com>
2024-12-10 11:50:01 +0530

    Delete day-1 directory

--- day-1/even-or-odd.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-    int num;
-    printf("Enter any number:");\
-    scanf("%d",&num);
-    (num%2==0)?printf("%d is an even number",num):printf("%d is an odd number",num);
-}    
